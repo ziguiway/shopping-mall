20298e64ac3c8c2f8def2627746b37bfecceff19
ziguiway <ziguiway@163.com>
2024-04-08 09:33:53 +0800

    完成登录静态页面，接口崩了项目暂时停止

--- src/utils/vant-ui.js
+++ src/utils/vant-ui.js
@@ -1,8 +1,9 @@
 import Vue from "vue";
-import { Button,Tabbar, TabbarItem,Icon  } from "vant";
+import { Button,Tabbar, TabbarItem,Icon ,NavBar  } from "vant";
 import "vant/lib/index.css";
 
 Vue.use(Button);
 Vue.use(Tabbar);
 Vue.use(Icon);
 Vue.use(TabbarItem);
+Vue.use(NavBar);
